4229eaf8949b2c338e407dd8c03e789839c9d3a5
TavinScapi <tavinhoscapi@gmail.com>
2025-03-09 01:34:45 -0300

    v2

--- script.js
+++ script.js
@@ -20,4 +20,15 @@ function menuBtnChange() {
     }
 }
 
+
 menuBtnChange();
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    e.target.matches('.next') && slider.append(items[0])
+    e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener('click', activate, false);
