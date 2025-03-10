10c364fd7fef41f88e880a285de8376858332843
TavinScapi <tavinhoscapi@gmail.com>
2025-03-10 01:39:01 -0300

    v3

--- script.js
+++ script.js
@@ -32,3 +32,44 @@ function activate(e) {
 }
 
 document.addEventListener('click', activate, false);
+
+
+function toggleDropdown() {
+    let dropdown = document.getElementById("dropdown");
+    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+}
+
+function filterGenres() {
+    let input = document.getElementById("searchGenre").value.toLowerCase();
+    let items = document.getElementsByClassName("dropdown-item");
+
+    for (let item of items) {
+        let text = item.innerText.toLowerCase();
+        item.style.display = text.includes(input) ? "block" : "none";
+    }
+}
+
+function selectGenre(value) {
+    document.querySelector(".dropdown-button").innerText = value.charAt(0).toUpperCase() + value.slice(1);
+    document.getElementById("dropdown").style.display = "none";
+    filterByGenre(value);
+}
+
+function filterByGenre(selectedGenre) {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        if (selectedGenre === 'todos' || card.getAttribute('data-genre') === selectedGenre) {
+            card.style.display = 'block'; // Exibe o cartão
+        } else {
+            card.style.display = 'none'; // Oculta o cartão
+        }
+    });
+}
+
+document.addEventListener("click", function (event) {
+    if (!event.target.closest(".filtro-generos")) {
+        document.getElementById("dropdown").style.display = "none";
+    }
+});
+
